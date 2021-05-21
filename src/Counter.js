128b6705860a2f8a7ead7cf67f1d75a21e3e074a
DungTNT <tuandungbrse@gmail.com>
2021-05-21 23:34:16 +0700

    finish

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { connect } from "react-redux";
-// Container - Presentation
+
+import { useSelector, useDispatch } from "react-redux";
 
 import {
   incrementActionCreator,
@@ -8,36 +8,24 @@ import {
   resetActionCreator,
 } from "./redux/actions/constants";
 
-const Counter = ({ count, increment, decrement, reset }) => {
-  console.log(count);
+const Counter = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="Counter">
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() => increment()}>Increment</button>
-        <button onClick={() => decrement()}>Decrement</button>
-        <button onClick={() => reset()}>Reset</button>
+        <button onClick={() => dispatch(incrementActionCreator())}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrementActionCreator())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(resetActionCreator())}>Reset</button>
       </section>
     </div>
   );
 };
 
-const mapStateToProps = (state) => state;
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch(incrementActionCreator()),
-    decrement: () => dispatch(decrementActionCreator()),
-    reset: () => dispatch(resetActionCreator()),
-  };
-};
-
-// const mapDispatchToProps = {
-//   increment: incrementActionCreator,
-//   decrement: decrementActionCreator,
-//   reset: resetActionCreator,
-// };
-
-const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
-
-export default CounterContainer;
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import CounterContainer from "./Counter";
+import Counter from "./Counter";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -9,7 +9,7 @@ const Application = () => {
   return (
     <main className="Application">
       <section className="Counters">
-        <CounterContainer />
+        <Counter />
       </section>
     </main>
   );

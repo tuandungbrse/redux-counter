650ec678bc39808ee3a8d18cdcedcb08e2a008f9
DungTNT <tuandungbrse@gmail.com>
2021-05-21 22:45:54 +0700

    counter using pure react

---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
+};
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Counter from "./Counter";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Application = () => {
+  return (
+    <main className="Application">
+      <section className="Counters">
+        <Counter />
+      </section>
+    </main>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Application />, document.getElementById("root"));
